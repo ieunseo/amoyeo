086d5969bca54d4b704e3ac612d354a1074b3d3e
ohdyo <smart5572@naver.com>
2024-06-01 22:59:25 +0900

    엔티티 클래스 작성

---
+++ spring-last-team/src/main/java/com/ohgiraffers/springlastteam/SpringLastTeamApplication.java
@@ -0,0 +1,13 @@
+package com.ohgiraffers.springlastteam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringLastTeamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringLastTeamApplication.class, args);
+    }
+
+}
---
+++ spring-last-team/src/test/java/com/ohgiraffers/springlastteam/SpringLastTeamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ohgiraffers.springlastteam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringLastTeamApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
