1beb8a1a3143b39e3d1d1d6f7514d22397a7528c
ddoddo1228 <135615995+ddoddo1228@users.noreply.github.com>
2024-06-03 11:36:55 +0900

    content.js

---
+++ src/main/resources/static/js/content.js
@@ -0,0 +1,9 @@
+//게시글 공구탭에서만 보이게
+function showPosts(element) {
+    document.querySelector('.post-container').style.display = 'block';
+}
+
+function hidePosts() {
+    document.querySelector('.post-container').style.display = 'none';
+}
+
