dd86fd684fa90c18a7afed22bd6bc618a11777d9
k168ww <k168ww@gmail.com>
2024-06-05 09:49:38 +0900

    #7 관리자 프론트 구현

--- src/main/java/com/ohgiraffers/springlastteam/controller/MainController.java
+++ src/main/java/com/ohgiraffers/springlastteam/controller/MainController.java
@@ -5,10 +5,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MainController {
+
     @GetMapping(value = {"/"})
     public String main() {
         return "index";
     }
+
     @GetMapping(value = {"/mypage"})
     public String mypage() {
         return "mypage";
---
+++ src/main/resources/static/js/admin.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const deleteButtons = document.querySelectorAll('.delete-button');
+
+    deleteButtons.forEach(button => {
+        button.addEventListener('click', function() {
+            const row = this.closest('tr');
+            const userId = row.querySelector('td:nth-child(2)').textContent;
+
+            if (confirm(`Are you sure you want to delete user ${userId}?`)) {
+                /* 추가 예정*/
+                row.remove();
+            }
+        });
+    });
+});
