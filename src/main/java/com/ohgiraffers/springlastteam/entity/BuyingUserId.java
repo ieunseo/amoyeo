604db0aff98e8ddf808a2404c572b29a7bec4dc3
k168ww <k168ww@gmail.com>
2024-06-07 10:17:12 +0900

    #13

--- src/main/java/com/ohgiraffers/springlastteam/entity/BuyingUserId.java
+++ src/main/java/com/ohgiraffers/springlastteam/entity/BuyingUserId.java
@@ -1,12 +1,15 @@
 package com.ohgiraffers.springlastteam.entity;
 
 
-import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import lombok.*;
 
 import java.io.Serializable;
 
+/* 임베더블 클래스입니다 복합키를 표현해줍니다.*/
 @Embeddable
 @Getter
 @Setter
@@ -15,9 +18,16 @@ import java.io.Serializable;
 @ToString
 public class BuyingUserId implements Serializable {
 
-    @Column(name = "buying_no")
-    private int buyingNo;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name="buying_no")
+    private  GroupBuying buyingNo;
 
-    @Column(name = "user_no")
-    private int userNo;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name="user_no")
+    private Users userNo;
+
+    public BuyingUserId(GroupBuying buyingNo, Users userNo) {
+        this.buyingNo = buyingNo;
+        this.userNo = userNo;
+    }
 }
--- src/main/java/com/ohgiraffers/springlastteam/entity/GroupBuying.java
+++ src/main/java/com/ohgiraffers/springlastteam/entity/GroupBuying.java
@@ -5,6 +5,8 @@ import lombok.*;
 
 import java.io.Serializable;
 
+/* 단체 구매 공고 게시글의 테이블입니다.
+ *  외래키로 유저의 고유번호(=관리자의 번호)를 가져와서 작성자가 누군지 확인 가능합니다.*/
 @Entity
 @Table(name = "group_buying")
 @Getter
@@ -26,16 +28,21 @@ public class GroupBuying implements Serializable {
 
     @Column(name = "buying_item")
     private String buyingItem;
+    @Column(name = "buying_quality")
+    private String buyingQuality;
 
+    @Column(name = "buying_price")
+    private int buyingPrice;
     @ManyToOne
     @JoinColumn(name = "user_no", nullable = false)
     private Users user;
 
-    public GroupBuying(Integer buyingNo, String buyingTitle, String buyingText, String buyingItem, Users user) {
-        this.buyingNo = buyingNo;
+    public GroupBuying(int buyingNo, String buyingTitle, String buyingText, String buyingItem, String buyingQuality, int buyingPrice, Users user) {        this.buyingNo = buyingNo;
         this.buyingTitle = buyingTitle;
         this.buyingText = buyingText;
         this.buyingItem = buyingItem;
+        this.buyingQuality = buyingQuality;
+        this.buyingPrice = buyingPrice;
         this.user = user;
     }
 }
