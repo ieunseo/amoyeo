8e2f88ac859a1f3ba8472c6b0895cfbb2b7e08c3
ddoddo1228 <135615995+ddoddo1228@users.noreply.github.com>
2024-06-11 21:49:27 +0900

    버튼 수정~.`

---
+++ src/main/resources/static/js/addpost.js
@@ -0,0 +1,9 @@
+function updateFileList() {
+    var input = document.getElementById('image');
+    var output = document.getElementById('fileList');
+    var children = "";
+    for (var i = 0; i < input.files.length; ++i) {
+        children += '<li>' + input.files.item(i).name + '</li>';
+    }
+    output.innerHTML = '<ul>' + children + '</ul>';
+}
