2ab617f550a9fae212cac2153db62f661d665cd5
ddoddo1228 <135615995+ddoddo1228@users.noreply.github.com>
2024-06-12 15:46:32 +0900

    엄청난 수정 및 추가

--- src/main/java/com/ohgiraffers/springlastteam/controller/MainController.java
+++ src/main/java/com/ohgiraffers/springlastteam/controller/MainController.java
@@ -13,5 +13,8 @@ public class MainController {
 
     @GetMapping(value={"/prices"})
     public String price() {return "price";}
-
+    @GetMapping(value = {"/contact"})
+    public String contact(){return "info/contact";}
+    @GetMapping(value = {"/terms"})
+    public String terms(){return "document/terms";}
 }
