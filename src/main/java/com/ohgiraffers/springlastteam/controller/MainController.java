6337078ab52122044b31864af3941be87bbb9a99
ieunseo <y949512@naver.com>
2024-06-14 09:20:48 +0900

    이용약관추가

--- src/main/java/com/ohgiraffers/springlastteam/controller/MainController.java
+++ src/main/java/com/ohgiraffers/springlastteam/controller/MainController.java
@@ -13,8 +13,13 @@ public class MainController {
 
     @GetMapping(value={"/prices"})
     public String price() {return "price";}
+
     @GetMapping(value = {"/contact"})
     public String contact(){return "info/contact";}
+
     @GetMapping(value = {"/terms"})
     public String terms(){return "document/terms";}
+
+    @GetMapping(value = {"/law"})
+    public String law(){return "document/yak";}
 }
